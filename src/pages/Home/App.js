2ac6043668b00519f8a4efc6c92c3a8752d43d85
Kelson <kelsonbatista@gmail.com>
2024-10-05 23:23:14 -0300

    Added assets

--- src/pages/Home/App.js
+++ src/pages/Home/App.js
@@ -1,10 +1,11 @@
-import './styles.js';
+import logo from '../../assets/images/github.png';
+import { Container } from './styles';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">Hello</header>
-    </div>
+    <Container>
+      <img src={logo} width={72} height={72} alt="Github logo" />
+    </Container>
   );
 }
 
---
+++ src/pages/Home/styles.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    display: flex;
+    align-items: flex-start;
+    justify-content: center;
+    width: 100%;
+    height: 90vh;
+    margin: 20px 0;
+  `
